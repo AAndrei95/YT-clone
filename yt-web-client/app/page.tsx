823c25662790467b47ded32bba0bfa0b947d22b7
Andrei <aalexandru518@gmail.com>
2026-08-10 16:29:55 +0100

    Created next.js app

---
+++ yt-web-client/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert h-5 w-[100px]"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+      </main>
+  );
+}
