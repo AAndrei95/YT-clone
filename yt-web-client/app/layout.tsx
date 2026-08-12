6ccd41fa8419af9798b97058e2c9e6e626a85a74
Andrei <aalexandru518@gmail.com>
2026-08-12 19:41:27 +0100

    Added Sign In with Google

--- yt-web-client/app/layout.tsx
+++ yt-web-client/app/layout.tsx
@@ -23,10 +23,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-      <Navbar />
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}  
+      </body>
     </html>
   );
 }
