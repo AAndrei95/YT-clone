f1d77d439c9bc1172635c424b677d29c87bd23fd
Andrei <aalexandru518@gmail.com>
2026-08-26 15:23:58 +0100

    Created upload video function

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -2,24 +2,15 @@ import * as functions from "firebase-functions/v1";
 import * as logger from "firebase-functions/logger";
 import {initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
+import {getStorage} from "firebase-admin/storage";
+import {onCall, HttpsError} from "firebase-functions/v2/https";
 
 initializeApp();
 
 const firestore = getFirestore("yt-clone-43ebc");
+
+const rawVideoBucketName =
+  process.env.RAW_VIDEO_BUCKET_NAME || "aa-yt-raw-videos";
 
 export const createUser = functions
   .region("europe-west2")
@@ -34,4 +25,34 @@ export const createUser = functions
     firestore.collection("users").doc(user.uid).set(userInfo);
     logger.info(`User Created: ${JSON.stringify(userInfo)}`);
     return;
+  });
+
+export const generateUploadUrl = onCall(
+  {
+    region: "europe-west2",
+    maxInstances: 1,
+  }, async (request) => {
+    // Check if user is authenticated
+    if (!request.auth) {
+      throw new HttpsError(
+        "failed-precondition",
+        "The function must be called while authenticated."
+      );
+    }
+
+    const storage = getStorage();
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // Generate unique filename
+    const fileName =
+      `${request.auth.uid}-${Date.now()}.${request.data.fileExtension}`;
+
+    // Get a v4 signed URL from upload file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return {url, fileName};
   });
